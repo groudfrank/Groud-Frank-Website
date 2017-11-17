7bd0146ffcc98b289cb1544c4249a3f2bb1fc4ff
Groud Frank X202e <groudfrank@gmail.com>
2017-11-17 15:04:55 -0400

    Some more fine tuning to the UI was done. I also removed javascript files that were not being used. Also added more check to the site-check.js file. I'll test if things are working when I get home

--- js/gf-vue.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    
-    var app2 = new Vue({
-        el: '.last-updated',
-        data: {
-          message: 'You loaded this page on ' + new Date().toLocaleString()
-        }
-      });
-});
--- js/site-checks.js
+++ js/site-checks.js
@@ -1,6 +1,25 @@
-$(document).ready{
-    if(is.chrome()){
-        // alert("eeewwww...ie");
-        window.location.href = "internet-explorer-sucks.html";
-      }
+$(document).ready(function(){
+var val = 0;
+var ie_visit = Window.sessionStorage.setItem("count", 0);
+
+if(is.ie() && ie_visit.count == 0){
+    window.location.href = "internet-explorer-sucks.html";
+   };
+
+if(is.chrome('<=45')){
+    window.location.href = "obsolete-browser.html";
 }
+
+if(is.firefox('<=49')){
+    window.location.href = "obsolete-browser.html";
+}
+
+if(is.ie('<=10')){
+    window.location.href = "obsolete-browser.html";
+}
+
+if(is.safari('<=5')){
+    window.location.href = "obsolete-browser.html";
+}
+
+});
