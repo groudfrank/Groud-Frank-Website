f19923ab48f524370ce20f62f30160e54621ef30
Groud Frank <groudfrank@gmail.com>
2017-11-19 18:51:46 -0400

    Testing if the activeX and scripts complaints only come when I run the site on IE locally. I uncommented the noscript tag

--- js/site-checks.js
+++ js/site-checks.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-var val = 0;
-var ie_visit = Window.sessionStorage.setItem("count", 0);
-
-if(is.ie() && ie_visit.count == 0){
-    window.location.href = "internet-explorer-sucks.html";
-   };
-
-if(is.chrome('<=45')){
-    window.location.href = "obsolete-browser.html";
-}
-
-if(is.firefox('<=49')){
-    window.location.href = "obsolete-browser.html";
-}
-
-if(is.ie('<=10')){
-    window.location.href = "obsolete-browser.html";
-}
-
-if(is.safari('<=5')){
-    window.location.href = "obsolete-browser.html";
-}
-
-});
+$(function(){
+    var val = 0;
+    var ie_visit = sessionStorage.setItem("count", 0);
+    
+    if(is.ie()){
+        window.location.href = "internet-explorer-sucks.html";
+        alert("IE count registered");
+       };
+    
+    if(is.chrome('<=45')){
+        window.location.href = "obsolete-browser.html";
+    };
+    
+    if(is.firefox('<=49')){
+        window.location.href = "obsolete-browser.html";
+    };
+    
+    if(is.ie('<=10')){
+        window.location.href = "obsolete-browser.html";
+    };
+    
+    if(is.safari('<=5')){
+        window.location.href = "obsolete-browser.html";
+    };
+})
